cfff7730dade7ecd13e6d70d51623e552804e80b
Bruno Alla <alla.brunoo@gmail.com>
2019-09-19 22:09:56 +0100

    chore(example): Change name in manifest

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Gallery example page`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
