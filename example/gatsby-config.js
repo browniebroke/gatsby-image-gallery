080f5e56b6d489017629d664bf3f70d7cde4d56e
Bruno Alla <alla.brunoo@gmail.com>
2019-09-19 21:48:18 +0100

    docs(example): Redo the example part to actually build

---
+++ example/gatsby-config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteMetadata: {
+    title: `Example for Gallery`,
+    description: `Just a simple example page to demo the gatsby-image-gallery.`,
+    author: `@browniebroke`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
+}
---
+++ example/src/pages/index.js
@@ -0,0 +1,49 @@
+import { graphql } from 'gatsby'
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Gallery from '@browniebroke/gatsby-image-gallery'
+import '@browniebroke/gatsby-image-gallery/dist/style.css'
+
+const IndexPage = ({data}) => {
+  const fullSize = data.images.edges.map(edge => edge.node.full.fluid.src)
+  const thumbs = data.images.edges.map(edge => edge.node.thumb.fluid)
+  return (
+    <Layout>
+      <SEO title="Example"/>
+      <h1>Gatsby image gallery demo</h1>
+      <p>A very simple page to demo the gallery component.</p>
+      <Gallery images={fullSize} thumbs={thumbs}/>
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+
+export const pageQuery = graphql`
+  query ImagesForGallery {
+    images: allFile(filter: { sourceInstanceName: { eq: "gallery" } }) {
+      edges {
+        node {
+          id
+          thumb: childImageSharp {
+            fluid(maxWidth: 270, maxHeight: 270) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+          full: childImageSharp {
+            fluid(
+              maxWidth: 1024
+              quality: 100
+              srcSetBreakpoints: [576, 768, 992, 1200]
+            ) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`
