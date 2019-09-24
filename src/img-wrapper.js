e76088bf3d75a36c4006e118a07b8592ce2b8d3b
Bruno Alla <alla.brunoo@gmail.com>
2019-09-24 22:44:49 +0100

    feat(styles): Replace Bootstrap CSS by styled-components
    BREAKING CHANGE: The API to customise the <Gallery/> component changed the colClass attribute is replaced by colWidth & mdColWidth.

---
+++ src/img-wrapper.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const ImgWrapper = styled.div`
+  margin: ${props => props.margin};
+`
+
+export default ImgWrapper
---
+++ src/row.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-right: -15px;
+  margin-left: -15px;
+`
+export default Row
