86d6001ced41c5d0ad94d8d5423351c21cf6d5ba
Bruno Alla <alla.brunoo@gmail.com>
2020-06-01 09:57:53 +0100

    fix(packaging): Use path.resolve for packaging CSS

--- gatsby-image-gallery/rollup.config.js
+++ gatsby-image-gallery/rollup.config.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
@@ -5,7 +6,6 @@ import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
 import svgr from '@svgr/rollup'
-
 import pkg from './package.json'
 
 export default {
@@ -25,7 +25,7 @@ export default {
   plugins: [
     external(),
     postcss({
-      extract: 'dist/style.css',
+      extract: path.resolve('dist/style.css'),
     }),
     url(),
     svgr(),
